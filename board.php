f1a93aa56cc7e41849293430a4f4eb4901e07621
Steven Meyer <meyer.s@husky.neu.edu>
2017-11-26 17:10:29 -0500

    ddddddddd
    d

--- board.php
+++ board.php
@@ -4,10 +4,8 @@ html{font-family:century gothic}
 </style>
 <body>
 <h1> Northeastaern Community Events</h1>
-
-<?php
-   echo str_replace('  ', '&nbsp; ', nl2br(print_r($_POST, true)));
-?>	
+	
+<?php echo $_POST["title"]; ?>
 
 </body>
 </html>
